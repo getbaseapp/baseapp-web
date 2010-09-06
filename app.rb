7dbfa8072dc37d046fdfc4ee0853606356bc1adc
Maximilian Schoening <resetability@gmail.com>
2010-09-06 18:00:40 +0200

    checking the receiver

--- app.rb
+++ app.rb
@@ -30,8 +30,8 @@ helpers do
 
     if purchase == 'VERIFIED'
       expected = {
-        :payment_status => 'Completed'
-        #:receiver_email => STORE_CONFIG[:paypal][:form][:business],
+        :payment_status => 'Completed',
+        :receiver_email => STORE_CONFIG[:paypal][:form][:business]
         #:mc_gross => STORE_CONFIG[:paypal][:form][:amount].to_s
       }
       expected.keys.all? { |key| params[key] == expected[key] }
